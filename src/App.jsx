362fe2d4fe9e19d1284a4cb92fb4eea31f82c7c9
Guilherme Santos <106110470+guileals@users.noreply.github.com>
2025-07-24 19:16:26 +0000

    Aula 24/07 - useContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,21 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { ProductList } from "./components/ProductList";
 import { Header } from "./components/Header";
-import { useState } from "react";
 import { Route, Routes } from "react-router";
 import { Cart } from "./components/Cart";
+import { CartProvider } from "./service/CartContext";
 
 export default function App() {
-  
-  const [cart, setCart] = useState([]);
-
-  function addToCart(product) {
-    setCart((prevCart) => [...prevCart, product]);
-  }
 
   return (
-    //React Fragment
     <>
-      <Header cart={cart} />
-      <Routes>
-        <Route path="/" element={<ProductList addToCart={addToCart} />} />
-        <Route path="/cart" element={<Cart cart={cart} />} />
-      </Routes>
+      <CartProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </CartProvider>
     </>
   );
 }
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,10 @@
 import styles from "./Cart.module.css";
+import { useContext } from "react";
+import { CartContext } from "../service/CartContext";
 
-export function Cart({ cart }) {
+export function Cart() {
+  const { cart, updateQtyCart, clearCart } = useContext(CartContext);
+
   return (
     <div className={styles.cart}>
       <h2>Shopping Cart</h2>
@@ -9,10 +13,27 @@ export function Cart({ cart }) {
       ) : (
         <ul>
           {cart.map((product, index) => (
-            <li key={index}>
+            <li key={index} className={styles.cartItem}>
               <img src={product.thumbnail} alt={product.title} />
               <h3>{product.title}</h3>
               <p>${product.price.toFixed(2)}</p>
+              <div className={styles.quantityControls}>
+                <button
+                  onClick={() =>
+                    updateQtyCart(product.id, product.quantity - 1)
+                  }
+                >
+                  -
+                </button>
+                <span>{product.quantity}</span>
+                <button
+                  onClick={() =>
+                    updateQtyCart(product.id, product.quantity + 1)
+                  }
+                >
+                  +
+                </button>
+              </div>
             </li>
           ))}
         </ul>
